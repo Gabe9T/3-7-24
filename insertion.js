ae31fea36493c6273bdd55d101945ec474f5ecf3
gabe <gabrieltucker260@gmail.com>
2024-03-07 09:33:48 -0800

    example

--- insertion.js
+++ insertion.js
@@ -1,2 +1,19 @@
-
+let array = [7, 3, 5, 9, 2, 6];
+function insertionSort(array) {
+    // Start on  second element because the first element is considered sorted
+    for (let i = 1; i < array.length; i++) {
+      let currentElement = array[i];
+      let j = i - 1; // index last 
   
+      // Move elements of the sorted section that are greater than the current element
+      // one position ahead of their current position
+      while (j >= 0 && array[j] > currentElement) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      // Insert the current element into its correct position in the sorted section
+      array[j + 1] = currentElement;
+    }
+  
+    return array;
+  }
